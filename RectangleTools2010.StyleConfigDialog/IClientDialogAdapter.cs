e5201c1909ba54c6e57f68159e751d7910180689
Brian Hall <Brian@Qube-it.com>
2011-08-22 20:06:13 -0400

    Added comment to the IClientDialogAdapter interface

--- RectangleTools2010.StyleConfigDialog/IClientDialogAdapter.cs
+++ RectangleTools2010.StyleConfigDialog/IClientDialogAdapter.cs
@@ -1,5 +1,9 @@
 ﻿namespace StyleConfigDialog
 {
+    /// <summary>
+    /// This is how the AddIn project interacts gets information from the dialog box.
+    /// This interface is implemented in the "General" namespace in the "ClientDialogAdapter" class.
+    /// </summary>
     public interface IClientDialogAdapter
     {
         bool PanelLayout { get; set; }
